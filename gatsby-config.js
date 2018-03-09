bf9a5dd2fa27a21a4683dab36b86d2cc592a6705
Devin Metivier <devinmetivier@gmail.com>
2018-03-08 22:20:25 -0500

    added proper gatsby prefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
         title: 'Gatsby Default Starter',
         desc: 'A new Blog',
     },
-    pathPrefix: '/gatsby',
+    pathPrefix: '/levelupgatsby',
     plugins: [
         'gatsby-plugin-react-helmet',
         'gatsby-plugin-styled-components',
